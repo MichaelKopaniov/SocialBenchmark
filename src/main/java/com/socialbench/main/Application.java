eac57793ff8eb11b6d4ea4e378d2229c6199a62f
Misha Kopaniov <michaelk@exelate.com>
2016-03-10 09:24:43 +0200

    initial commit

---
+++ src/main/java/com/socialbench/main/Application.java
@@ -0,0 +1,16 @@
+package com.socialbench.main;
+
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.FlatMapFunction;
+import org.apache.spark.api.java.function.Function;
+
+import java.io.Serializable;
+import java.util.Arrays;
+
+public class Application {
+
+	public static void main(String[] args) throws Exception {
+
+	}
+}
